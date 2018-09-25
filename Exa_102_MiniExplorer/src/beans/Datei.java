8133e22976232bfea5474cd2d3cc597d91ceee0a
Fabian Zach <zacfac15@new.edvhtl.at>
2018-09-25 09:45:07 +0200

    Edit

--- Exa_102_MiniExplorer/src/beans/Datei.java
+++ Exa_102_MiniExplorer/src/beans/Datei.java
@@ -1,7 +1,17 @@
-
 package beans;
 
-public class Datei
+import java.io.File;
+import java.text.SimpleDateFormat;
+
+public class Datei extends File
 {
-  
+
+  private static final SimpleDateFormat SDF = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+  private String displayName;
+
+  public Datei(String pathname)
+  {
+    super(pathname);
+    displayName = getName();
+  }
 }
